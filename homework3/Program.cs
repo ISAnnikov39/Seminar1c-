b2c882655bfdc0f677d030bf03b8f7fe1f2a1caa
Konstantin <pkv39konstantin@yandex.ru>
2022-10-06 14:59:18 +0300

    did example 3

---
+++ homework3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Введите любое из чисел '4', '-3', '7'");
+int a = Convert.ToInt32(Console.ReadLine());
+ while (true)
+ {
+    if (a==4 | a == -3 | a== 7  ) {
+       if  (a % 2 == 0){ 
+        Console.WriteLine("четное число");
+        break;}
+        else{ 
+            Console.WriteLine("нечетное число");
+            break;
+        }
+    }
+    else if (a!=4 | a!=-3 | a != 7) {
+            Console.WriteLine("Извините, другие числа мы не проверяем");
+   }
+   return;
+ }
+
+
